7896a2d0b44cd423d7b1efc01ecdc225ea48bf28
Gaurish <gaurish.official@gmail.com>
2024-02-18 11:19:09 +0530

    update code

--- BookStoreApp/src/app/books/components/add-book-reactive/add-book-reactive.component.ts
+++ BookStoreApp/src/app/books/components/add-book-reactive/add-book-reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { BookService } from '../../services/book.service';
 
 @Component({
@@ -8,10 +8,11 @@ import { BookService } from '../../services/book.service';
   styleUrls: ['./add-book-reactive.component.css']
 })
 export class AddBookReactiveComponent implements OnInit {
-  
+
   public addBookForm: FormGroup;
+  public titleErrorMessage: string;
   currencies : any[] = [{value:'USD',viewValue:'US Dollar'},{value:'INR',viewValue:'Indial Rupes'}];
-  constructor(private _bookService: BookService) {
+  constructor(private _bookService: BookService,private _formBuilder: UntypedFormBuilder) {
     
   }
   ngOnInit(): void {
@@ -20,21 +21,70 @@ export class AddBookReactiveComponent implements OnInit {
   }
   private initForm()
   {
-    this.addBookForm = new FormGroup({
-      title: new FormControl(null,Validators.required),
-      author: new FormControl('Gaurish',[Validators.required,Validators.minLength(10)]), // Set default value
-      totalPages: new FormControl(),
-      price: new FormGroup({ currency: new FormControl(),value: new FormControl() }),
-      publishedOn: new FormControl(),
-      isPublished: new FormControl(),
-      formatType: new FormControl(),
-      pdfFormat: new FormControl(),
-      docFormat: new FormControl()
+    this.addBookForm = this._formBuilder.group({
+      title: ['this is default', [Validators.required, Validators.minLength(10)]],
+       author: '',
+      totalPages: '',
+      price: this._formBuilder.group({
+        currency: '',
+        value: ''
+      }),
+      publishedOn: '',
+      isPublished: '',
+      // formatType: '',
+      // pdfFormat: '',
+      // docFormat: '',
+      //authors: this._formBuilder.array([this.getAuthorControl(), this.getAuthorControl()])
     });
+    // this.addBookForm = new FormGroup({
+    //   title: new FormControl(null,Validators.required),
+    //   author: new FormControl('Gaurish',[Validators.required,Validators.minLength(10)]), // Set default value
+    //   totalPages: new FormControl(),
+    //   price: new FormGroup({ currency: new FormControl(),value: new FormControl() }),
+    //   publishedOn: new FormControl(),
+    //   isPublished: new FormControl(),
+    //   formatType: new FormControl(),
+    //   pdfFormat: new FormControl(),
+    //   docFormat: new FormControl()
+    //});
   }
   saveBook(): void {
     console.log(this.addBookForm.value);
     if(this.addBookForm.valid)
         this._bookService.addBook(this.addBookForm.value);
   }
+  // private getAuthorControl(): UntypedFormGroup {
+  //   return this._formBuilder.group({
+  //     fullName: '',
+  //     address: ''
+  //   });
+  // }
+
+
+  // private validateTitleControl(titleControl: UntypedFormControl): void {
+  //   this.titleErrorMessage = '';
+  //   if (titleControl.errors && (titleControl.touched || titleControl.dirty)) {
+  //     if (titleControl.errors?.required) {
+  //       this.titleErrorMessage = 'This is a required field';
+  //     } else if (titleControl.errors?.minlength) {
+  //       this.titleErrorMessage = 'Minimum length is ' + titleControl.errors?.minlength?.requiredLength;
+  //     }
+  //   }
+  // }
+
+  // private formatTypeChanged(formatType: string): void {
+  //   const pdfControl = this.addBookForm.get('pdfFormat');
+  //   const docControl = this.addBookForm.get('docFormat');
+
+  //   if (formatType === 'pdf') {
+  //     pdfControl?.addValidators([Validators.required, Validators.minLength(10)]);
+  //     docControl?.clearValidators();
+  //   } else if (formatType === 'doc') {
+  //     docControl?.addValidators(Validators.required);
+  //     pdfControl?.clearValidators();
+  //   }
+
+  //   pdfControl?.updateValueAndValidity();
+  //   docControl?.updateValueAndValidity();
+  // }
 }
